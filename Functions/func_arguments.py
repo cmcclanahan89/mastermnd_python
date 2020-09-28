60208d71d7e55aafadf624df0325190cf3575790
Cory <cmcclanahan89@gmail.com>
2020-09-28 01:34:38 -0400

    adding more files for function practice

---
+++ Functions/func_arguments.py
@@ -0,0 +1,12 @@
+#take 2 numbers and print out the sum
+def add(x,y):
+    sum = x + y
+    print(sum)
+
+def subtract(x,y):
+    difference = x - y
+    print(difference)
+
+
+add(21,35)
+subtract(108,27)
